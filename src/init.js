0bdfb6a68099bd7d4646836522aee2be14082dad
elise <115508722+cultelise@users.noreply.github.com>
2022-11-05 02:13:41 -0500

    Add content factory (list)

--- src/init.js
+++ src/init.js
@@ -1,20 +1,55 @@
 const content = document.getElementById('content'), 
   divElements = [];
 
-const divObjector = (div, parent, text, divClass = '') => {
+const listFactory = (type, listItemNumber) => {
+  if (type === 'unordered'.toLowerCase()) {
+    const unorderedList = document.createElement('ul');
+    let counter = 0;
+    while (counter < listItemNumber) {
+      const listItem = document.createElement('li');
+      listItem.id = `li${counter + 1}`;
+      unorderedList.appendChild(listItem);
+      counter++;
+    } return unorderedList;
+  } else {
+      const orderedList = document.createElement('ol');
+      let counter = 0;
+      while (counter < listItemNumber) {
+        const listItem = document.createElement('li');
+        listItem.id = `li${counter + 1}`;
+        orderedList.appendChild(listItem);
+        counter++;
+      } return orderedList;
+    }
+}
+
+const divFactory = (div, parent, text, divClass = '', addList, type, num) => {
   const createdDiv = document.createElement('div');
   createdDiv.id = div;
   createdDiv.textContent = text;
   createdDiv.className = divClass;
+  
+  if (addList === 'yes' || addList === true) {
+    let list = listFactory(type, num);
+    createdDiv.appendChild(list);
+  }
+  
+
   return {name: div, parent, createdDiv}
 }
 
-const pageTitle = divObjector('pageTitle', content, 'Restaurant Menu'),
-  pageBody = divObjector('pageBody', content),
-  leftBody = divObjector('leftBody', pageBody.createdDiv, undefined, 'pageBodySection'),
-  rightBody = divObjector('rightBody', pageBody.createdDiv, undefined, 'pageBodySection');
+const 
+  pageTitle = divFactory('pageTitle', content, 'Restaurant Menu'),
+  pageBody = divFactory('pageBody', content),
+  leftBody = divFactory('leftBody', pageBody.createdDiv, undefined, 'pageBodySection'),
+  rightBody = divFactory('rightBody', pageBody.createdDiv, undefined, 'pageBodySection'),
+  leftTitle = divFactory('leftTitle', leftBody.createdDiv, 'Left Title'),
+  rightTitle = divFactory('rightTitle', rightBody.createdDiv, 'Right Title'),
+  leftListX = divFactory('leftList', leftBody.createdDiv, undefined, '', true, 'unordered', 5);
 
-const divMaker = (...divObject) => {
+  console.log(leftListX)
+
+const appendDiv = (...divObject) => {
   divObject.forEach(div => {
     const divElement = div.createdDiv;
     div.parent.appendChild(divElement);
@@ -22,4 +57,7 @@ const divMaker = (...divObject) => {
   });
 }
 
-const divGroup = divMaker(pageTitle, pageBody, leftBody, rightBody)
+
+appendDiv(pageTitle, pageBody, leftBody, rightBody, leftTitle, rightTitle, leftListX);
+
+
